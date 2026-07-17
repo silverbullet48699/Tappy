f215e697df38cd8995c6400a77cab8ed961d3ce9
Stephanie Vania Suwardi <stphnivsuwardi09@gmail.com>
2026-07-17 11:15:52 +0700

    add Nav bar

--- Tappy/ContentView.swift
+++ Tappy/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TabView{
+                
+                Tab("Home", systemImage: "house.fill"){
+                    HomeView()
+                }
+                Tab("Reminder", systemImage: "bell.fill"){
+                    ReminderView()
+                }
+                
+                
+                Tab("Calender", systemImage: "calendar"){
+                    HomeView()
+                }
+                
+                
+                
+            }
         }
         .padding()
     }
