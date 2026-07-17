0f42163bf7b150a3389fc24835f5f301eae5dd4d
Stephanie Vania Suwardi <stphnivsuwardi09@gmail.com>
2026-07-17 10:59:30 +0700

    Add View Page and folder
    add view

---
+++ Tappy/Model/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Tappy
+//
+//  Created by Stephanie Vania Suwardi Data on 17/07/26.
+//
+
---
+++ Tappy/View/CalenderIView.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderIView.swift
+//  Tappy
+//
+//  Created by Stephanie Vania Suwardi Data on 17/07/26.
+//
+
+import SwiftUI
+
+struct CalenderIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderIView()
+}
