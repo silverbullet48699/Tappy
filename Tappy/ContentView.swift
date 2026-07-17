f335ab00a23d2887de5ee6c2a07731d6746a7f91
Stephanie Vania Suwardi <stphnivsuwardi09@gmail.com>
2026-07-17 10:46:05 +0700

    Initial Commit

---
+++ Tappy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tappy
+//
+//  Created by Stephanie Vania Suwardi Data on 17/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tappy/TappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TappyApp.swift
+//  Tappy
+//
+//  Created by Stephanie Vania Suwardi Data on 17/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
